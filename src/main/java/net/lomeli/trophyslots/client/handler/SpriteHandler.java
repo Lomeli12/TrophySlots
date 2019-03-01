eaea5661442d4b3ce0cb00c24d7df9b7d420b2c1
Lomeli12 <anthonylomeli11@gmail.com>
2019-03-01 00:53:41 -0800

    Only register the cross sprite once on the sprite atlas.
    This should stop MC from complaining about missing the cross sprite for paintings, mob effects, and particles.

--- src/main/java/net/lomeli/trophyslots/client/handler/SpriteHandler.java
+++ src/main/java/net/lomeli/trophyslots/client/handler/SpriteHandler.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.lomeli.trophyslots.TrophySlots;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
@@ -12,6 +13,9 @@ public class SpriteHandler {
     public static final Identifier CROSS_SPRITE = new Identifier(TrophySlots.MOD_ID, "gui/cross");
 
     public static void stitchSprites() {
-        ClientSpriteRegistryCallback.EVENT.register((atlasTexture, registry) -> registry.register(CROSS_SPRITE));
+        ClientSpriteRegistryCallback.EVENT.register((atlasTexture, registry) -> {
+            if (atlasTexture == MinecraftClient.getInstance().getSpriteAtlas())
+                registry.register(CROSS_SPRITE);
+        });
     }
 }
